8ad4cf858fa3575ca799b1e74da206dd0ea91691
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-19 17:39:36 -0300

    remove tours not interested button

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
-  const removeTours = (id) => {
+  const removeTour = (id) => {
     const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
   };
 
   const fetchTours = async () => {
@@ -38,7 +39,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
