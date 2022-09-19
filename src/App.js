217e40f9a3fdae3ea72f1dd1f5f389a0b0bd2ba1
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-09-19 17:46:46 -0300

    refresh button

--- src/App.js
+++ src/App.js
@@ -37,6 +37,18 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button onClick={fetchTours} className="btn">
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
